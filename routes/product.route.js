279705088328efe456781bfb252dee7553c77171
ShareefRahat <imr4h47@gmail.com>
2022-09-24 19:12:14 +0600

    sequence changed and bulkUpdateService is added

--- routes/product.route.js
+++ routes/product.route.js
@@ -8,14 +8,14 @@ router
   .get(productController.getProduct)
   .post(productController.createProduct);
 
-//-----------Update and Delete--------------\\
+//-----------Bulk-Update and Bulk-Delete--------------\\
+router.route("/bulk-update").patch(productController.bulkUpdateProduct);
+router.route("/bulk-delete").delete(productController.bulkDeleteProduct);
+
+//-----------Single Update and Delete--------------\\
 router
   .route("/:id")
   .patch(productController.updateProduct)
   .delete(productController.deleteProduct);
-
-//-----------Bulk-Update and Bulk-Delete--------------\\
-router.route("/bulk-update").patch(productController.bulkUpdateProduct);
-router.route("/bulk-delete").delete(productController.bulkDeleteProduct);
 
 module.exports = router;
